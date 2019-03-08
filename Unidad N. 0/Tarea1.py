f7ace96e03418eff6de51c941f9d2bf65495fcb2
unknown <josemanuelcarrillo.dev@gmail.com>
2019-03-07 23:03:28 -0600

    tarea, adelanto

--- Unidad N. 0/Tarea1.py
+++ Unidad N. 0/Tarea1.py
@@ -6,7 +6,8 @@ Jose Manuel Carrillo Angulo
 """
 import numpy as np
 
-# Ejercicio 1
+##########   Ejercicio 1   ##########
+ 
 # a) Resultado
 
 # ---------- (1) -------------
@@ -57,3 +58,30 @@ y = 4 * np.pi
 
 z = np.math.sqrt(x**2 + y**2)
 print(z)
+
+##########   Ejercicio 2   ##########
+
+# a) listas
+
+x = np.array([0.3, -5.31, -1, -9, 10, 0, 18, 90])
+y = np.array([9, 1, 1, -3, 1, -99, -10, 10, -7, -1])
+
+# b) Calcule la media, la varianza y la desviacion estandar de y.
+
+mediaY = np.mean(y)
+varianzaY = np.var(y)
+desviacionEstY = np.std(y)
+
+print("Media de Y: " + str(mediaY))
+print("Varianza de Y: " + str(varianzaY))
+print("Desviacion estandar de Y: " + str(desviacionEstY))  
+
+# b) Calcule la media, la varianza y la desviacion estandar de x.
+
+mediaX = np.mean(x)
+varianzaX = np.var(x)
+desviacionEstX = np.std(x)
+
+print("Media de X: " + str(mediaX))
+print("Varianza de X: " + str(varianzaX))
+print("Desviacion estandar de X: " + str(desviacionEstX))
