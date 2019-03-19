a356f2f32aeffc5780c1477740139110c4e371fb
Carrillo Angulo Jose Manuel <JoCarr3@ice.go.cr>
2019-03-19 16:58:03 -0600

    realizado hasta ejercicio 5 de la unidad 2

--- Unidad N. 2/Tarea2_Jose_Carrillo.py
+++ Unidad N. 2/Tarea2_Jose_Carrillo.py
@@ -1,15 +1,59 @@
+# -*- coding: UTF-8 -*-
+
 #%% [markdown]
 # ## Ejercicio 1
-# Programe en Python una funci´on que recibe dos valores, determinar cual de los dos valores es 
+# Programe en Python una función que recibe dos valores, determinar cual de los dos valores es 
 # el mayor y luego lo retorna (no puede usar la funci´on max de Python).
 
 #%%
-import os
-
 import ciencia_datos as cd
+import importlib
+importlib.reload(cd)
 
 a = 5
 b = 7
 
-print(cd.ObtenerMayor(a, b))
+print('El Mayor entre ' + str(a) + ' y ' + str(b) + ' es: ' + str(cd.obtenerMayor(a, b)))
 
+#%% [markdown]
+# ## Ejercicio 2
+# Programe en Python una funcion que recibe tres valores A, B, y C y retorna el mayor (no puede
+# usar la funcion max de Python)
+#%%
+A = 11
+B = 9
+C = 67
+
+print('El Mayor entre ' + str(A) + ' , ' + str(B) + ' y ' + str(C) + ' es: ' + str(cd.obtenerMayor3(A, B, C)))
+
+#%% [markdown]
+# ## Ejercicio 3
+# Programe en Python una funci´on que recibe cuatro n´umeros y retorna el mayor (no puede usar
+# la funci´on max de Python).
+#%%
+W = 100
+X= 200
+Y = 670
+Z = 810
+
+print('El Mayor entre ' + str(W) + ' , ' + str(X) + ' , ' + str(Y) + ' y ' + str(Z) + ' es: ' + str(cd.obtenerMayor4(W, X, Y, Z)))
+
+#%% [markdown]
+# ## Ejercicio 4
+# Programe en Python una funci´on que recibe un n´umero n y retorna la sumatoria de los n´umeros
+# enteros al cuadrado comprendidos entre el 1 y el n.
+#%%
+n = 4
+print('La sumatoria de enteros al cuadrado de 1 hasta ' + str(n) + ' es: ' + str(cd.sumatoria(n)))
+
+#%% [markdown]
+# ## Ejercicio 5
+# Desarrolle una funci´on que realice la sumatoria de los n´umeros enteros m´ultiplos de 3, comprendidos entre el 1 y el n.
+#%%
+n = 9
+print('La sumatoria de enteros multiplos de 3 desde 1 hasta ' + str(n) + ' es: ' + str(cd.sumatoriaMult3(n)))
+
+#%% [markdown]
+# ## Ejercicio 6
+# Programe en Python una funci´on que genera 2000 n´umeros al azar entre 1 y 5000 y luego calcula
+# cu´antos est´an entre el 1500 y 4500, ambos inclusive.
--- Unidad N. 2/ciencia_datos.py
+++ Unidad N. 2/ciencia_datos.py
@@ -3,12 +3,14 @@ Modulo de funciones de ciencia de datos
 @autor Jose Manuel Carrillo
 """
 
-def ObtenerMayor(a ,b):
-    """Devuelve el numero mayor
+def obtenerMayor(a ,b):
+    """Devuelve el numero mayor de dos argumentos, si son iguales devuelve None
     
     Arguments:
         a {[int]} 
         b {[int]}
+    Returns:
+        int | None
     """
 
     if (a > b):
@@ -18,5 +20,76 @@ def ObtenerMayor(a ,b):
     else:
         return None
 
+def obtenerMayor3(a, b, c):
+    """Devuelve el numero mayor de tres argumentos, si son iguales devuelve None
+    
+    Arguments:
+        a {[int]} 
+        b {[int]}
+        c {[int]}
+    Returns:
+        int | None
+    """
 
+    if(a == b and b == c):
+        return None
+    elif (a > b and a > c):
+        return a
+    elif(b > c):
+        return b
+    else:
+        return c
 
+def obtenerMayor4(a, b, c, d):
+    """Devuelve el numero mayor de cuatro argumentos, si son iguales devuelve None
+    
+    Arguments:
+        a {[int]} 
+        b {[int]}
+        c {[int]}
+        d {[int]}
+    Returns:
+        int | None
+    """
+    if(a == b and b == c and c == d):
+        return None
+    elif(a > b and a > c and a > d):
+        return a
+    elif(b > c and b > d):
+        return b
+    elif(c > d):
+        return c
+    else:
+        return d
+
+def sumatoria(n):
+    """Devuelve la sumatoria de numeros enteros de 1 hasta n
+    
+    Arguments:
+        n {int} -- límite de sumatoria
+    """
+    sum = 0
+    for i in range(1, n+1):
+        sum = sum + i**2
+    return sum
+
+def sumatoriaMult3(n):
+    """Devuelve la sumatoria de numeros enteros multiplos de 3 de 1 hasta n
+    
+    Arguments:
+        n {int} -- límite de sumatoria
+    """
+    sum = 0
+    for i in range(1, n+1):
+        if(i%3 == 0):
+            sum += i
+    return sum
+
+def randomRango(cantidad, entre, ):
+    """Genera una cantidad de numeros al azar y devuelve
+    
+    Arguments:
+        bajo {[type]} -- [description]
+        alto {[type]} -- [description]
+        cantidad {[type]} -- [description]
+    """
